f34457fefc2c81106841bb4dd4658c08ba017875
Vladimir089 <pickerpikcer@gmail.com>
2024-02-27 00:05:18 +0300

    top

--- Drink Water Reminder App/Controller/ViewController.swift
+++ Drink Water Reminder App/Controller/ViewController.swift
@@ -7,16 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+var targetDrink: Float = 3500
+var dailyDrink: Float = 2000
 
+class ViewController: UIViewController {
+    
     var mainView: MainView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         mainView = MainView(frame: view.frame)
         self.view = mainView
+        mainView?.slider?.addTarget(self, action: #selector(sliderValueChanged(_:)), for: .valueChanged)
     }
-
-
+    
+    
+    @objc func sliderValueChanged(_ sender: UISlider) {
+        targetDrink = sender.value
+        self.mainView?.targetLabel?.text = "\(Int(targetDrink)) ml"
+        mainView?.progressView?.setProgress(dailyDrink / targetDrink, animated: true)
+        if dailyDrink > targetDrink {
+            mainView?.progressView?.progressTintColor = .systemRed
+        } else {
+            mainView?.progressView?.progressTintColor = .white
+        }
+    }
+    
+    
 }
 
--- Drink Water Reminder App/View/MainView.swift
+++ Drink Water Reminder App/View/MainView.swift
@@ -10,6 +10,13 @@ import SnapKit
 
 class MainView: UIView {
 
+    //MARK: -Elements
+    
+    var dailyLabel: UILabel?
+    var targetLabel: UILabel?
+    var progressView: UIProgressView?
+    var slider: UISlider?
+    
     override init(frame: CGRect) {
         super.init(frame:frame)
         backgroundColor = UIColor(red: 248/255, green: 248/255, blue: 248/255, alpha: 1)
@@ -20,7 +27,7 @@ class MainView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func createComponents() {
+    private func createComponents() {
         
         //MARK: -Background
         
@@ -52,7 +59,94 @@ class MainView: UIView {
         
         //MARK: -Other
         
+        let dailyDrinkTargetLabel: UILabel = {
+            let label = UILabel()
+            label.text = "Daily Drink Target"
+            label.font = .systemFont(ofSize: 20, weight: .light)
+            label.textColor  = UIColor(red: 240/255, green: 240/255, blue: 240/255, alpha: 1)
+            return label
+        }()
+        addSubview(dailyDrinkTargetLabel)
+        dailyDrinkTargetLabel.snp.makeConstraints { make in
+            make.left.equalToSuperview().inset(30)
+            make.top.equalToSuperview().inset(70)
+        }
         
+    dailyLabel = {
+        let label = UILabel()
+        label.text = "\(Int(dailyDrink))"
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 50, weight: .medium)
+        return label
+    }()
+        addSubview(dailyLabel ?? UILabel())
+        dailyLabel?.snp.makeConstraints({ make in
+            make.top.equalTo(dailyDrinkTargetLabel).inset(20)
+            make.left.equalTo(dailyDrinkTargetLabel)
+        })
+        
+        let separatorLabel: UILabel = {
+            let label = UILabel()
+            label.text = "/"
+            label.textColor = UIColor(red: 240/255, green: 240/255, blue: 240/255, alpha: 1)
+            label.font = .systemFont(ofSize: 20, weight: .medium)
+            return label
+        }()
+        addSubview(separatorLabel)
+        separatorLabel.snp.makeConstraints { make in
+            make.right.equalTo(dailyLabel!).inset(-10)
+            make.bottom.equalTo(dailyLabel!).inset(12)
+        }
+        
+        targetLabel = {
+            let label = UILabel()
+            label.text = "\(Int(targetDrink)) ml"
+            label.textColor = .white
+            label.font = .systemFont(ofSize: 20, weight: .light)
+            return label
+        }()
+        addSubview(targetLabel ?? UILabel())
+        targetLabel?.snp.makeConstraints({ make in
+            make.left.equalTo(separatorLabel).inset(10)
+            make.bottom.equalTo(separatorLabel).inset(-2)
+        })
+        
+        progressView = {
+            let progress = UIProgressView(progressViewStyle: .bar)
+            progress.trackTintColor = UIColor(red: 127/255, green: 243/255, blue: 166/255, alpha: 1)
+            progress.layer.cornerRadius = 5
+            progress.layer.sublayers?[1].cornerRadius = 5
+            progress.clipsToBounds = true
+            progress.subviews[1].clipsToBounds = true
+            progress.setProgress(dailyDrink / targetDrink, animated: true)
+            return progress
+        }()
+
+        addSubview(progressView ?? UIProgressView())
+        addSubview(progressView ?? UIProgressView())
+        progressView?.snp.makeConstraints({ make in
+            make.left.right.equalToSuperview().inset(30)
+            make.height.equalTo(10)
+            make.top.equalTo(separatorLabel.snp.bottom).inset(-40)
+        })
+        
+        slider = {
+            let slider = UISlider()
+            slider.maximumValue = 5000
+            slider.value = targetDrink
+            slider.minimumValue = 0
+            slider.tintColor = .clear
+            slider.maximumTrackTintColor = .clear
+            slider.setThumbImage(UIImage(named: "triangle.fill"), for: .normal)
+            return slider
+        }()
+        addSubview(slider ?? UISlider())
+        slider?.snp.makeConstraints({ make in
+            make.left.right.equalToSuperview().inset(30)
+            make.height.equalTo(20)
+            make.top.equalTo(progressView!.snp.bottom)
+
+        })
     }
 
 }
