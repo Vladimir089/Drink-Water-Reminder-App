d42f37d62c06f775f1847ea2402d1326691264c6
Vladimir089 <pickerpikcer@gmail.com>
2024-02-26 14:03:08 +0300

    Initial Commit

---
+++ Drink Water Reminder App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Drink Water Reminder App
+//
+//  Created by Владимир Кацап on 26.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
